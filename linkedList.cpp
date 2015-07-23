420bd92ea96d1b19ea4a3df55336625a53f8f16a
Jordan Schultz <jschultz11@gmail.com>
2015-07-23 10:29:34 -0400

    revisioned with xcode + insert function

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,32 +1,60 @@
+//
+//  LinkedList.cpp
+//  
+//
+//  Created by Jordan Schultz on 7/23/15.
+//
+//
+
+#include <stdio.h>
+
+
 /*
-  Print elements of a linked list on console 
-  head pointer input could be NULL as well for empty list
-  Node is defined as 
-  struct Node
-  {
-     int data;
-     struct Node *next;
-  }
-*/
-void Print(Node *head)
+ Insert Node at the end of a linked list
+ head pointer input could be NULL as well for empty list
+ Node is defined as
+
+struct Node
 {
-  Node* placement = head;
-  int sum = 0;
+    int data;
+    struct Node *next;
+}
+*/
+
+
+Node* Insert(Node *head,int data)
+{
+    Node* start = head;
+    Node* insert = new Node;
+    insert->data = data;
     
-  while(placement)
-  {
-      if( placement->data)
-          cout<< placement->data<<endl;;
-         
-      
-      placement = placement->next;
-  }//while
-    
-  
-    
-    
+    if(!head)
+    {
+        return insert;
+    }else{
+        
+        while(head->next)
+            head = head->next;
+        
+        head->next = insert;
+        
+        return start;
+        
+    }//else
 }
 
-
-
-
+void Print(Node *head)
+{
+    Node* placement = head;
+    int sum = 0;
+    
+    while(placement)
+    {
+        if( placement->data)
+            cout<< placement->data<<endl;;
+        
+        
+        placement = placement->next;
+    }//while
+    
+}
